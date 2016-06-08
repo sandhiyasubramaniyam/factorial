fe05c20453c5c1941619f70fa1b196fce2879e5d
sandhiyasubramaniyam <sandhiya.cs13@bitsathy.ac.in>
2016-06-08 10:49:54 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,21 @@
+number=input("Enter a number ") 
+
+sum=1
+
+if(number==0):
+
+ print("1")
+
+elif(number==1):
+ 
+ print("1")
+
+else:
+
+ while(number>0):
+
+  sum=sum*number
+  
+  number=number-1
+ 
+  print(sum)
